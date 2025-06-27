7c0393fde6ed2ce351d08bcfb5f6abb923db99f4
James Hordon <jehordon@gmail.com>
2025-06-27 16:33:50 +0100

    Removing unnecessary methods from functions.py

--- functions.py
+++ functions.py
@@ -1,23 +1,5 @@
 FILEPATH = 'todos.txt'
 
-def print_hi(name):
-    """
-    :param name:
-    :return: Prints a message to the name argument.
-    """
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-def print_todo(todos_arg):
-    """
-    :param todos_arg:
-    :return: Take a todo list and prints out each element.
-    """
-    for index, todo in enumerate(todos_arg):
-        todo = todo.strip('\n')
-        print(f"{index + 1}.{todo}")
-
 
 def write_todos(todo_arg, filepath=FILEPATH):
     """
@@ -40,30 +22,6 @@ def get_todos(filepath=FILEPATH):
         todos_arg = [todo.replace("\n", "") for todo in todos_arg]
     return todos_arg
 
-
-def validate_selection(todos_arg):
-    """
-    :param todos_arg:
-    :return: Validates that the item number selected is contained
-        in the to-do list index and returns the item number.
-    """
-    validation_check = True
-    while validation_check:
-        try:
-            item_num_arg = int(input("Enter the item number you would like to select: "))
-            selected_todo = todos_arg[item_num_arg - 1].strip('\n')
-            user_validation = input(f"You have chosen: '{selected_todo}' \nIs this correct? [y/n]")
-            match user_validation:
-                case 'y':
-                    break
-                case 'n':
-                    validate_selection(todos_arg)
-                    break
-                case _:
-                    print("Hey, you entered an unknown command!")
-        except (ValueError, IndexError):
-            print("You entered an incorrect value!")
-    return item_num_arg
 
 if __name__ == '__main__':
     print("Hello from functions!")
